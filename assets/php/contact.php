536bdb648beb7384e7ab96b38d8072183c1eda5e
Zoheir ZIANI <zoheir.business@gmail.com>
2021-01-21 12:31:06 +0100

    Update contact.php

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,12 +1,19 @@
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$tel = $_POST['tel']
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "ziani.zoheir@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel']
+    $message = $_POST['message'];
+
+    $recipient = "ziani.zoheir@gmail.com";
+    $subject = "You have received an email";
+    $formcontent="From: $name \n Message: $message";
+    
+    
+    $mailheader = "From: $email \r\n";
+    
+    mail($recipient, $subject, $formcontent, $mailheader);
+    header("Location: index.html?mailsend")
+
+}
 ?>

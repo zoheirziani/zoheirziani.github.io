bd68896eedc97fdbecc7cf871358a1ba2059eae9
Zoheir ZIANI <zoheir.business@gmail.com>
2021-01-21 12:33:59 +0100

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,14 @@
 function change() {
-    var x = document.getElementById("user");
+    var x = document.getElementById("name");
     x.value = x.value.toUpperCase();
+}
+
+function validate() {
+    var n1 = document.getElementById("name");
+    var n2 = document.getElementById("email");
+    if(n1.value !="" && n2.value != ""){
+        return true;
+    }
+    alert("You should give your name and your email");
+    return false;
 }
